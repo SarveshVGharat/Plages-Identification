307fe5caac524eb92f9f3b0212a94e71a80cb795
abhimanyu911 <abhimanyuborthakur@hotmail.com>
2023-05-06 18:22:40 +0530

    added rectified plots

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ client = bigquery.Client(credentials = credentials)
 #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(st.secrets["gcp_service_account"])
 df = pd.read_csv('./plage_areas.txt', header=None, skiprows=34, names=["Year", "Month", "Day", "Projected Area", "Corrected Area"], delim_whitespace = True)
 project_id = st.secrets["gcp_service_account"]["project_id"]+'.'+st.secrets["database_id"]["db_name"]
-cycle_id = 22
+cycle_id = 16
 images_dir_prefix = './images-cycle-'
 output_image_dir_prefix = './output-images-cycle-'
 table_id = project_id + '.' + 'cycle-' + str(cycle_id)
